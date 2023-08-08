59877c592d5ad3b62ba025dae62d95c507e6d2dd
mohagussetiaone <mohagussetiaone@gmail.com>
2023-08-08 14:37:40 +0700

    update port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import upload from "../uploads/upload";
 import { createBarang, getBarang, getBarangById, updateBarang, deleteBarang } from "../controllers/barangController";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 app.use(express.json());
 
