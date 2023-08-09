1c382647ea434b9e69baa662a71402d543d4ffe7
mohagussetiaone <mohagussetiaone@gmail.com>
2023-08-09 21:25:43 +0700

    update cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const upload = require("./uploads/upload.js");
+const cors = require("cors");
 const { createBarang, getBarang, getBarangById, updateBarang, deleteBarang } = require("./controllers/barangController");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/barang", upload.single("foto_barang"), createBarang);
